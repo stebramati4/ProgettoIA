6a6fdfa8c80938a90bd3edfcab952c4e154ae939
stebramati4 <s.bramati2@campus.unimib.it>
2023-02-23 16:48:24 +0100

    pulizia codice

--- PYTHON/main.py
+++ PYTHON/main.py
@@ -1,13 +1,21 @@
 import numpy as np
 import funzioni as f
 
+
+# PREDISPOSIZIONE INIZIALE
+
+#definizione dimensione labirinto
 n = 5
 
+#predisposizione del labirinto
 matrix = f.creaLabirinto(n)
 f.stampaMatrice(matrix)
 print()
 NP = np.array(matrix)
 
+#creazione tabella Manhattan
 manhattan = f.manhattan(NP)
 print()
 f.stampaMatrice(manhattan)
+
+#INIZIO DEL GIOCO

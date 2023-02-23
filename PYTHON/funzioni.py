0571a63adaa539b09594ed70e905cd2b31443a6a
stebramati4 <s.bramati2@campus.unimib.it>
2023-02-23 16:43:59 +0100

    manhattan funzionante

--- PYTHON/funzioni.py
+++ PYTHON/funzioni.py
@@ -1,26 +1,49 @@
-def manhattan(NP):
+import numpy as np
+import random
 
-    manhattan = []
+#crea il labirinto nella situazione iniziale
+def creaLabirinto(n):
+    matrix = []
+
+    for i in range(n):
+        row = []
+        if i == 0:
+            # scegli casualmente tra "G1", "G2" e "V" per ogni cella della prima riga
+            scelteR1 = ["G1", "G2"] + ["V"] * (n - 2)
+            random.shuffle(scelteR1)
+            row = scelteR1
+        if i == n - 1:
+            scelteRUltimo = ["M"] + ["V"] * (n - 1)
+            random.shuffle(scelteRUltimo)
+            row = scelteRUltimo
+        matrix.append(row)
+
+    # fa array con n*n-2 celle, da usare nella popolazione del corpo
+    scelteCorpo = (n - 1) * ["L"] + ["P"] * (n - 2) + (n - 3) * ["F"] + ["I"] + (n * (n - 2) - 3 * (n - 2) - 1) * ["V"]
+    random.shuffle(scelteCorpo)
+
+    # popola il corpo della matrice con Invurgus, Ostacoli e Funghi
+    for i in range(1, n - 1):
+        matrix[i] = scelteCorpo[:5]
+        scelteCorpo = scelteCorpo[5:]
+
+    return matrix
+
+
+# crea la matrice manhattan con le distanze di ogni cella del labirinto al goal più vicino
+def manhattan(NP):
+    manhattan = np.array(NP)
     G1 = trovaGoal(NP, "G1")
     G2 = trovaGoal(NP, "G2")
 
     for i in range(NP.shape[0]):
         for j in range(NP.shape[1]):
-            print(NP[i, j])
             manhattan[i][j] = distanzaGoalPiùVicino(G1, G2, i, j)
 
-    stampaMatrice(manhattan)
+    return manhattan
 
 
-    # for ogni cella
-        # calcola il goal più vicino
-        # trova cella goal
-        # calcola distanza dal goal
-        # assegnazione
-
-    #return G1 #matrice manhattan
-
-
+#restituisce l'indice y della prima riga, corrispondente alla posizione del goal
 def trovaGoal (matrix, goal):
     n = len(matrix[0])
 
@@ -29,24 +52,31 @@ def trovaGoal (matrix, goal):
     for i in range(n):
         if firstRow[i] == goal:
             cella = i
+
     return cella
 
 
 #ritorna la distanza minima
 def distanzaGoalPiùVicino(G1, G2, x, y):
+
     # calcola manhattan dalla cella ai due nodi
     distanzaG1 = distanza(0, G1, x, y)
     distanzaG2 = distanza(0, G2, x, y)
+
     if distanzaG1 < distanzaG2:
         return distanzaG1
     else:
         return distanzaG2
 
+
+# calcola la distanza tra due elementi della matrice
 def distanza(x1, y1, x2, y2):
     # distanza = | x(mago) - x(invurgus) | + | y(mago) - y(invurgus) |
     distanza = abs(x1 - x2) + abs(y1 - y2)
-    print(distanza)
+
     return distanza
+
+
 
 # stampa la matrice
 def stampaMatrice(matrix):
--- PYTHON/main.py
+++ PYTHON/main.py
@@ -1,33 +1,13 @@
 import numpy as np
-import random
 import funzioni as f
 
 n = 5
-matrix = []
 
-for i in range(n):
-    row = []
-    if i == 0:
-        # scegli casualmente tra "G1", "G2" e "V" per ogni cella della prima riga
-        scelteR1 = ["G1", "G2"] + ["V"] * (n - 2)
-        random.shuffle(scelteR1)
-        row = scelteR1
-    if i == n-1:
-        scelteRUltimo = ["M"] + ["V"] * (n - 1)
-        random.shuffle(scelteRUltimo)
-        row = scelteRUltimo
-    matrix.append(row)
-
-# fa array con n*n-2 celle, da usare nella popolazione del corpo
-scelteCorpo = (n-1)*["L"] + ["P"] * (n - 2) + (n-3)*["F"] + ["I"] + (n*(n-2) - 3*(n-2)-1)*["V"]
-random.shuffle(scelteCorpo)
-
-# popola il corpo della matrice con Invurgus, Ostacoli e Funghi
-for i in range(1, n-1):
-    matrix[i] = scelteCorpo[:5]
-    scelteCorpo = scelteCorpo[5:]
-
+matrix = f.creaLabirinto(n)
 f.stampaMatrice(matrix)
+print()
 NP = np.array(matrix)
 
-f.manhattan(NP)
+manhattan = f.manhattan(NP)
+print()
+f.stampaMatrice(manhattan)

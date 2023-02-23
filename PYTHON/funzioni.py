4f29a14e9e374447057da2f92e150860ff516de2
stebramati4 <s.bramati2@campus.unimib.it>
2023-02-23 11:35:19 +0100

    Predisposizione Manhattan

---
+++ PYTHON/funzioni.py
@@ -0,0 +1,25 @@
+def manhattan(matrix):
+
+    manhattan = []
+    G1 = trovaGoal("G1")
+    G2 = trovaGoal("G2")
+
+    # for ogni cella
+        # calcola il goal più vicino
+        # trova cella goal
+        # calcola distanza dal goal
+        # assegnazione
+
+    return #matrice manhattan
+
+
+def trovaGoal (goal):
+    # cerca goal nella matrice
+    return # cella del goal
+def goalPiùVicino(G1, G2, cella):
+    # calcola manhattan dalla cella ai due nodi
+    return # cella del goal più vicino
+
+def distanza(cella1, cella2):
+    distanza = | x(mago) - x(invurgus) | + | y(mago) - y(invurgus) |
+    return #distanza
